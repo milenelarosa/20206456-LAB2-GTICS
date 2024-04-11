b45221a2d16f126931e92f91877d9392b32ca467
Aracelli La Rosa <a20206456@pucp.edu.pe>
2024-04-10 20:31:43 -0500

    first commit

---
+++ LAB_PATITOS/src/main/java/com/example/lab_patitos/LabPatitosApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_patitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPatitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPatitosApplication.class, args);
+	}
+
+}
